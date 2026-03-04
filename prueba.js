adc00b45f4142e41d024465a3fd0e82f0317573c
alejandroulloa <alejandroulloa417@gmail.com>
2026-03-04 09:11:27 -0500

    se agrego a archivo js

---
+++ prueba.js
@@ -0,0 +1,9 @@
+// Saludo bÃ¡sico en JavaScript
+
+let nombre = prompt("Â¿CuÃ¡l es tu nombre?");
+
+if (nombre) {
+    console.log("Hola " + nombre + " í±‹ Bienvenida/o al mundo de JavaScript");
+} else {
+    console.log("No ingresaste ningÃºn nombre.");
+}
