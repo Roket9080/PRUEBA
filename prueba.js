33d52abc90a1f0c66d2c4e3019e4b8fcd813d8f9
Liseth Munoz <lisethmunos234@gmail.com>
2026-03-04 09:18:06 -0500

    DESCARGADO

---
+++ prueba.js
@@ -0,0 +1,11 @@
+// Mostrar mensaje en la consola
+console.log("Hola Liseth, este es mi primer código en JavaScript");
+
+// Mostrar una alerta en pantalla
+alert("Bienvenida a JavaScript");
+
+// Crear una variable
+let nombre = "Liseth";
+
+// Mostrar la variable en consola
+console.log("Tu nombre es: " + nombre);
